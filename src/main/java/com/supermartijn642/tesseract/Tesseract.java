8011937c7aaf58b4b7c4eabac0398c25d51e1ee1
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-04-30 20:06:01 +0200

    update version

--- src/main/java/com/supermartijn642/tesseract/Tesseract.java
+++ src/main/java/com/supermartijn642/tesseract/Tesseract.java
@@ -25,7 +25,7 @@ public class Tesseract {
     public static final String MODID = "tesseract";
     public static final String NAME = "Tesseract";
     public static final String MC_VERSIONS = "[1.12.2]";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     public static SimpleNetworkWrapper channel;
 
