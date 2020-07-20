95f7ec544ca8b99c2d64e9c377f2872aeb0b1d49
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-07-20 09:37:38 +0200

    add private button tooltip

--- src/main/java/com/supermartijn642/tesseract/screen/TesseractScreen.java
+++ src/main/java/com/supermartijn642/tesseract/screen/TesseractScreen.java
@@ -178,6 +178,9 @@ public class TesseractScreen extends Screen {
         super.func_230430_a_(matrixStack, mouseX, mouseY, partialTicks);
         this.textField.func_230430_a_(matrixStack, mouseX, mouseY, partialTicks);
 
+
+        if(this.privateButton.func_230449_g_())
+            this.func_238654_b_(matrixStack, Collections.singletonList(new TranslationTextComponent("gui.tesseract.channel." + (this.privateButton.isLocked() ? "private" : "public"))), mouseX, mouseY);
         if(this.transferButton.func_230449_g_())
             this.func_238654_b_(matrixStack, Collections.singletonList(this.transferButton.state.translate()), mouseX, mouseY);
         if(this.redstoneButton.func_230449_g_())
