17d9b7dba2c40b625ffcbf15a8a9d5cb17aa370d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-12-17 22:11:45 +0100

    various performance improvements

---
+++ src/main/java/com/supermartijn642/tesseract/manager/TesseractTracker.java
@@ -0,0 +1,130 @@
+package com.supermartijn642.tesseract.manager;
+
+import com.supermartijn642.tesseract.TesseractTile;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.CompressedStreamTools;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.storage.FolderName;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created 12/16/2020 by SuperMartijn642
+ */
+public class TesseractTracker {
+
+    public static MinecraftServer minecraftServer;
+
+    public static final TesseractTracker SERVER = new TesseractTracker();
+//    public static final TesseractTracker CLIENT = new TesseractTracker();
+
+    public static TesseractTracker getInstance(World world){
+        return world.isRemote ? null /*CLIENT*/ : SERVER;
+    }
+
+    private final HashMap<String,HashMap<BlockPos,TesseractReference>> tesseracts = new HashMap<>();
+
+    public TesseractReference add(TesseractTile self){
+        String dimension = self.getWorld().getDimensionKey().getLocation().toString();
+        this.tesseracts.putIfAbsent(dimension, new HashMap<>());
+        return this.tesseracts.get(dimension).computeIfAbsent(self.getPos(), key -> new TesseractReference(self));
+    }
+
+    @Deprecated
+    public TesseractReference tryAdd(String dimension, BlockPos pos){
+        if(minecraftServer == null)
+            return null;
+
+        RegistryKey<World> key = RegistryKey.getOrCreateKey(Registry.WORLD_KEY, new ResourceLocation(dimension));
+        World world = TesseractChannelManager.minecraftServer.getWorld(key);
+        TileEntity tile = world.getTileEntity(pos);
+        return tile instanceof TesseractTile ? this.add((TesseractTile)tile) : null;
+    }
+
+    public TesseractReference remove(World world, BlockPos pos){
+        String dimension = world.getDimensionKey().getLocation().toString();
+        this.tesseracts.putIfAbsent(dimension, new HashMap<>());
+        return this.tesseracts.get(dimension).remove(pos);
+    }
+
+    public TesseractReference get(World world, BlockPos pos){
+        String dimension = world.getDimensionKey().getLocation().toString();
+        return this.tesseracts.putIfAbsent(dimension, new HashMap<>()).get(pos);
+    }
+
+    public CompoundNBT writeKey(TesseractReference reference){
+        CompoundNBT tag = new CompoundNBT();
+        tag.putString("dimension", reference.getDimension());
+        tag.putInt("posx", reference.getPos().getX());
+        tag.putInt("posy", reference.getPos().getY());
+        tag.putInt("posz", reference.getPos().getZ());
+        return tag;
+    }
+
+    public TesseractReference fromKey(CompoundNBT key){
+        String dimension = key.getString("dimension");
+        BlockPos pos = new BlockPos(key.getInt("posx"), key.getInt("posy"), key.getInt("posz"));
+        return this.tesseracts.containsKey(dimension) ?
+            this.tesseracts.get(dimension).get(pos) : null;
+    }
+
+    @SubscribeEvent
+    public static void onSave(WorldEvent.Save e){
+        if(e.getWorld().isRemote() || !(e.getWorld() instanceof World) || ((World)e.getWorld()).getDimensionKey() != World.OVERWORLD)
+            return;
+
+        File directory = new File(((ServerWorld)e.getWorld()).getServer().func_240776_a_(FolderName.DOT).toFile(), "tesseract/tracking");
+        int index = 0;
+        for(Map.Entry<String,HashMap<BlockPos,TesseractReference>> dimensionEntry : SERVER.tesseracts.entrySet()){
+            for(Map.Entry<BlockPos,TesseractReference> entry : dimensionEntry.getValue().entrySet()){
+                File file = new File(directory, "tesseract" + index + ".nbt");
+                try{
+                    file.getParentFile().mkdirs();
+                    file.createNewFile();
+                    CompressedStreamTools.write(entry.getValue().write(), file);
+                }catch(IOException ioException){
+                    ioException.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onLoad(WorldEvent.Load e){
+        if(e.getWorld().isRemote() || !(e.getWorld() instanceof World) || ((World)e.getWorld()).getDimensionKey() != World.OVERWORLD)
+            return;
+
+        minecraftServer = ((ServerWorld)e.getWorld()).getServer();
+
+        SERVER.tesseracts.clear();
+
+        File directory = new File(((ServerWorld)e.getWorld()).getServer().func_240776_a_(FolderName.DOT).toFile(), "tesseract/tracking");
+        File[] files = directory.listFiles();
+        if(files != null){
+            for(File file : directory.listFiles()){
+                if(file.isFile() && file.getName().endsWith(".nbt")){
+                    try{
+                        CompoundNBT tag = CompressedStreamTools.read(file);
+                        TesseractReference location = new TesseractReference(tag);
+                        SERVER.tesseracts.putIfAbsent(location.getDimension(), new HashMap<>());
+                        SERVER.tesseracts.get(location.getDimension()).put(location.getPos(), location);
+                    }catch(IOException ioException){
+                        ioException.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/supermartijn642/tesseract/packets/PacketCompleteChannelsUpdate.java
@@ -0,0 +1,71 @@
+package com.supermartijn642.tesseract.packets;
+
+import com.supermartijn642.tesseract.ClientProxy;
+import com.supermartijn642.tesseract.EnumChannelType;
+import com.supermartijn642.tesseract.manager.Channel;
+import com.supermartijn642.tesseract.manager.TesseractChannelManager;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.fml.network.NetworkEvent;
+
+import java.util.*;
+import java.util.function.Supplier;
+
+/**
+ * Created 4/23/2020 by SuperMartijn642
+ */
+public class PacketCompleteChannelsUpdate {
+
+    private List<Channel> channels;
+
+    public PacketCompleteChannelsUpdate(){
+        List<Channel>[] lists = new List[EnumChannelType.values().length];
+        int index = 0;
+        int size = 0;
+        for(EnumChannelType type : EnumChannelType.values()){
+            lists[index] = TesseractChannelManager.SERVER.getChannels(type);
+            size += lists[index].size();
+            index++;
+        }
+        this.channels = new ArrayList<>(size);
+        for(List<Channel> list : lists)
+            this.channels.addAll(list);
+    }
+
+    private PacketCompleteChannelsUpdate(List<Channel> channels){
+        this.channels = channels;
+    }
+
+    public void encode(PacketBuffer buffer){
+        CompoundNBT compound = new CompoundNBT();
+
+        Iterator<Channel> iterator = this.channels.iterator();
+        for(int index = 0; iterator.hasNext(); index++)
+            compound.put(Integer.toString(index), iterator.next().writeClientChannel());
+
+        buffer.writeCompoundTag(compound);
+    }
+
+    public static PacketCompleteChannelsUpdate decode(PacketBuffer buffer){
+        CompoundNBT compound = buffer.readCompoundTag();
+
+        ArrayList<Channel> channels = new ArrayList<>();
+        for(String key : compound.keySet())
+            channels.add(Channel.readClientChannel(compound.getCompound(key)));
+
+        return new PacketCompleteChannelsUpdate(channels);
+    }
+
+    public void handle(Supplier<NetworkEvent.Context> ctx){
+        ctx.get().setPacketHandled(true);
+        ctx.get().enqueueWork(() -> {
+            TesseractChannelManager.CLIENT.clear();
+            Set<EnumChannelType> types = new HashSet<>(3);
+            this.channels.forEach(channel -> {
+                TesseractChannelManager.CLIENT.addChannel(channel);
+                types.add(channel.type);
+            });
+            types.forEach(type -> TesseractChannelManager.CLIENT.sortChannels(ClientProxy.getPlayer(), type));
+        });
+    }
+}
