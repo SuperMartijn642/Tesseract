4157641923da96eddfe634696dc9d7ed27c302d4
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-02-20 01:10:13 +0100

    Add integration with Jade and The One Probe

--- src/main/java/com/supermartijn642/tesseract/generators/TesseractLanguageGenerator.java
+++ src/main/java/com/supermartijn642/tesseract/generators/TesseractLanguageGenerator.java
@@ -19,6 +19,13 @@ public class TesseractLanguageGenerator extends LanguageGenerator {
         this.block(Tesseract.tesseract, "Tesseract");
         this.translation("tesseract.tesseract.info", "Items, fluids, and energy can be transferred between multiple tesseracts");
 
+        // Highlight info
+        this.translation("tesseract.tesseract.highlight.channels", "Channels:");
+        this.translation("tesseract.tesseract.highlight.channel_info", "  %1$s %2$s %3$s");
+        this.translation("tesseract.tesseract.highlight.channel_info.separator", "-");
+        this.translation("tesseract.tesseract.highlight.channel_info.inactive", "Inactive");
+        this.translation("tesseract.tesseract.highlight.redstone_blocked", "Blocked by redstone signal");
+
         // Screen
         this.translation("gui.tesseract.title", "Tesseract");
         this.translation("gui.tesseract.items", "TESSERACT - ITEMS");
@@ -56,5 +63,9 @@ public class TesseractLanguageGenerator extends LanguageGenerator {
         this.translation("gui.tesseract.info.tab.items", "Item Transport");
         this.translation("gui.tesseract.info.tab.fluid", "Fluid Transport");
         this.translation("gui.tesseract.info.tab.energy", "Energy Transport");
+
+        // Jade
+        this.translation("config.jade.plugin_tesseract.remove_default", "Tesseract - Remove default info");
+        this.translation("config.jade.plugin_tesseract.tesseract", "Tesseract - Info");
     }
 }
