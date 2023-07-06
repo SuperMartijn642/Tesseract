dca701f306af5f3971574f5089e4ea8835f22725
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-07-06 17:28:09 +0200

    Fix game profile loading in PlayerRenderer

--- src/main/java/com/supermartijn642/tesseract/screen/PlayerRenderer.java
+++ src/main/java/com/supermartijn642/tesseract/screen/PlayerRenderer.java
@@ -1,18 +1,16 @@
 package com.supermartijn642.tesseract.screen;
 
-import com.google.common.collect.Iterables;
 import com.google.gson.JsonObject;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import com.mojang.authlib.minecraft.MinecraftSessionService;
-import com.mojang.authlib.properties.Property;
 import com.supermartijn642.core.ClientUtils;
 import com.supermartijn642.core.gui.ScreenUtils;
 import net.minecraft.client.resources.DefaultPlayerSkin;
 import net.minecraft.client.resources.SkinManager;
-import net.minecraft.server.management.PlayerProfileCache;
 import net.minecraft.util.JSONUtils;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.UsernameCache;
 
 import javax.annotation.Nullable;
 import java.io.BufferedReader;
@@ -29,7 +27,7 @@ import java.util.UUID;
  */
 public class PlayerRenderer {
 
-    // TODO: this should probably be cleared after a certain time
+    // TODO this should probably be cleared after a certain time
     private static final Map<UUID,GameProfile> PLAYER_PROFILE_MAP = new HashMap<>();
     private static final HashSet<UUID> FETCH_QUEUE = new HashSet<>();
 
@@ -40,7 +38,7 @@ public class PlayerRenderer {
 
     public static String getPlayerUsername(UUID player){
         GameProfile profile = fetchPlayerProfile(player);
-        return profile == null ? null : profile.getName();
+        return profile == null ? UsernameCache.getLastKnownUsername(player) : profile.getName();
     }
 
     public static ResourceLocation getPlayerSkin(UUID player){
@@ -62,8 +60,7 @@ public class PlayerRenderer {
         }
 
         synchronized(FETCH_QUEUE){
-            if(!FETCH_QUEUE.contains(player)){
-                FETCH_QUEUE.add(player);
+            if(FETCH_QUEUE.add(player)){
                 new Thread(() -> {
                     boolean success = false;
                     String name = fetchPlayerName(player);
@@ -86,7 +83,7 @@ public class PlayerRenderer {
                     synchronized(FETCH_QUEUE){
                         FETCH_QUEUE.remove(player);
                     }
-                }, "Tesseract - UUID to username").start();
+                }, "Tesseract - Game profile fetching").start();
             }
         }
 
@@ -95,20 +92,9 @@ public class PlayerRenderer {
 
     @Nullable
     private static GameProfile updateGameProfile(@Nullable GameProfile input){
-        if(input != null && input.getName() != null && !input.getName().isEmpty()){
-            if(!input.isComplete() || !input.getProperties().containsKey("textures")){
-                PlayerProfileCache profileCache = getProfileCache();
-                MinecraftSessionService sessionService = getSessionService();
-                if(profileCache != null && sessionService != null){
-                    GameProfile gameprofile = profileCache.get(input.getName());
-                    if(gameprofile != null){
-                        Property property = Iterables.getFirst(gameprofile.getProperties().get("textures"), null);
-                        if(property == null)
-                            gameprofile = sessionService.fillProfileProperties(gameprofile, true);
-                        return gameprofile;
-                    }
-                }
-            }
+        if(input != null && input.getId() != null){
+            MinecraftSessionService sessionService = getSessionService();
+            return sessionService.fillProfileProperties(input, true);
         }
         return null;
     }
@@ -129,10 +115,6 @@ public class PlayerRenderer {
             }
         }catch(Exception ignore){}
         return null;
-    }
-
-    private static PlayerProfileCache getProfileCache(){
-        return ClientUtils.getMinecraft().getSingleplayerServer().getProfileCache();
     }
 
     private static MinecraftSessionService getSessionService(){
