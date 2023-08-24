de04157c9786f3b445a0bcf3f30df1667cbecffd
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-08-24 14:19:10 +0200

    Add recursion checks in FlatMapIterator #81

--- src/main/java/com/supermartijn642/tesseract/capabilities/FlatMapIterator.java
+++ src/main/java/com/supermartijn642/tesseract/capabilities/FlatMapIterator.java
@@ -1,7 +1,9 @@
 package com.supermartijn642.tesseract.capabilities;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * Created 16/04/2023 by SuperMartijn642
@@ -12,20 +14,43 @@ public class FlatMapIterator<S, T> implements Iterator<T> {
     private final Function<S,Iterator<T>> mapper;
     private Iterator<T> currentEntry;
 
-    public FlatMapIterator(Iterator<S> iterator, Function<S,Iterator<T>> mapper){
+    private final Supplier<Boolean> pushCall;
+    private final Runnable popCall;
+
+    public FlatMapIterator(Iterator<S> iterator, Function<S,Iterator<T>> mapper, Supplier<Boolean> pushCall, Runnable popCall){
         this.iterator = iterator;
         this.mapper = mapper;
+        this.pushCall = pushCall;
+        this.popCall = popCall;
+    }
+
+    public FlatMapIterator(Iterator<S> iterator, Function<S,Iterator<T>> mapper){
+        this(iterator, mapper, null, null);
     }
 
     @Override
     public boolean hasNext(){
+        if(this.pushCall != null && this.pushCall.get())
+            return false;
+
         while((this.currentEntry == null || !this.currentEntry.hasNext()) && this.iterator.hasNext())
             this.currentEntry = this.mapper.apply(this.iterator.next());
-        return this.currentEntry != null && this.currentEntry.hasNext();
+        boolean hasNext = this.currentEntry != null && this.currentEntry.hasNext();
+
+        if(this.popCall != null)
+            this.popCall.run();
+        return hasNext;
     }
 
     @Override
     public T next(){
-        return this.currentEntry.next();
+        if(this.pushCall != null && this.pushCall.get())
+            throw new NoSuchElementException();
+
+        T next = this.currentEntry.next();
+
+        if(this.popCall != null)
+            this.popCall.run();
+        return next;
     }
 }
